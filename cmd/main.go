c6131e99b3b8c70ee20d7ecb9977455278c26923
LemonLzy <32241375+LemonLzy@users.noreply.github.com>
2022-05-19 20:31:02 +0800

    增加配置文件初始化读取，增加gin demo示例。

--- cmd/main.go
+++ cmd/main.go
@@ -1,12 +1,21 @@
 package main
 
 import (
+	"fmt"
 	"github.com/gin-gonic/gin"
 	"github.com/lemonlzy/vegetableBlog/cmd/vegetableBlog"
 )
 
 func main() {
+	err := vegetableBlog.Init()
+	if err != nil {
+		fmt.Println(err)
+	}
+
 	engine := gin.New()
 	vegetableBlog.Register(engine)
-	engine.Run()
+	err = engine.Run(":" + vegetableBlog.Conf.Port)
+	if err != nil {
+		fmt.Println(err)
+	}
 }
---
+++ cmd/vegetableBlog/conf.go
@@ -0,0 +1,59 @@
+package vegetableBlog
+
+import (
+	"github.com/BurntSushi/toml"
+)
+
+var Conf = new(AppConfig)
+
+const (
+	configFilepath = "./configs/config.toml"
+)
+
+type AppConfig struct {
+	Name      string `toml:"name"`
+	Mode      string `toml:"mode"`
+	Port      string `toml:"port"`
+	Version   string `toml:"version"`
+	SinceTime string `toml:"since_time"`
+
+	*LogConfig   `toml:"log"`
+	*DBConfig    `toml:"db"`
+	*CacheConfig `toml:"cache"`
+}
+
+type LogConfig struct {
+	Level      string `toml:"level"`
+	Filename   string `toml:"filename"`
+	MaxSize    int    `toml:"max_size"`
+	MaxAge     int    `toml:"max_age"`
+	MaxBackups int    `toml:"max_backups"`
+}
+
+type DBConfig struct {
+	Host         string `toml:"host"`
+	User         string `toml:"user"`
+	Password     string `toml:"password"`
+	DBName       string `toml:"db_name"`
+	Port         int    `toml:"port"`
+	MaxOpenConns int    `toml:"max_open_conns"`
+	MaxIdleConns int    `toml:"max_idle_conns"`
+	TablePrefix  string `toml:"table_prefix"`
+	MigrateTable bool   `toml:"migrate_table"`
+}
+
+type CacheConfig struct {
+	Host     string `toml:"host"`
+	Port     int    `toml:"port"`
+	Password string `toml:"password"`
+	DB       int    `toml:"db"`
+	PoolSize string `toml:"pool_size"`
+}
+
+func Init() error {
+	_, err := toml.DecodeFile(configFilepath, &Conf)
+	if err != nil {
+		return err
+	}
+	return nil
+}
