7e5e2815c5d5736dd49cd68189d9f669df2f83de
LemonLzy <lzy291980138@163.com>
2022-06-10 16:45:47 +0800

    增加腾讯云COS对象存储

---
+++ internal/pkg/oss/oss.go
@@ -0,0 +1,48 @@
+package oss
+
+import (
+	"context"
+	"fmt"
+	"github.com/tencentyun/cos-go-sdk-v5"
+	"net/http"
+	"net/url"
+	"os"
+)
+
+var ossClient = genClient()
+
+func getCosID() string {
+	SecretID := os.Getenv("COS_SecretID")
+	fmt.Println(SecretID)
+	return SecretID
+}
+
+func getCosKey() string {
+	SecretKey := os.Getenv("COS_SecretKey")
+	fmt.Println(SecretKey)
+	return SecretKey
+}
+
+func genClient() *cos.Client {
+	// https://cloud.tencent.com/document/product/436/31215
+	u, _ := url.Parse("https://blog-1300597227.cos.ap-guangzhou.myqcloud.com")
+	su, _ := url.Parse("https://cos.COS_REGION.myqcloud.com")
+	b := &cos.BaseURL{BucketURL: u, ServiceURL: su}
+	client := cos.NewClient(b, &http.Client{
+		Transport: &cos.AuthorizationTransport{
+			SecretID:  getCosID(),
+			SecretKey: getCosKey(),
+		},
+	})
+	return client
+}
+
+func ossGet() {
+	result, _, _ := ossClient.Bucket.Get(context.Background(), nil)
+	fmt.Println(result)
+
+	//name := "./test/objectPut.go"
+	//f := strings.NewReader("test")
+	//put, _ := ossClient.Object.Put(context.Background(), name, f, nil)
+	//fmt.Println(put)
+}
